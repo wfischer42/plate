39b89c2da17d3bdb188ab7a78d2af007d10eefc9
zbeyens <ziad.beyens@gmail.com>
2023-04-19 20:13:18 +0200

    💄🔧

--- docs/config/tailwind.config.js
+++ docs/config/tailwind.config.js
@@ -1,5 +1,4 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   content: ['../src/**/*.html', '../src/**/*.js', '../src/**/*.tsx'],
@@ -10,17 +9,68 @@ module.exports = {
     extend: {
       colors: {
         'gray-850': '#18212F',
-        blue: '#28c6e4',
         red: '#f92672',
         darkBlue: '#8be9fd',
         darkPink: '#ff79c6',
         darkBodyBackground: '#272b36',
-        ...colors,
+
+        border: 'hsl(214.3 31.8% 91.4%)',
+        input: 'hsl(214.3 31.8% 91.4%)',
+        ring: 'hsl(215 20.2% 65.1%)',
+        background: 'hsl(0 0% 100%)',
+        foreground: 'hsl(222.2 47.4% 11.2%)',
+        primary: {
+          DEFAULT: 'hsl(222.2 47.4% 11.2%)',
+          foreground: 'hsl(210 40% 98%)',
+        },
+        secondary: {
+          DEFAULT: 'hsl(210 40% 96.1%)',
+          foreground: 'hsl(222.2 47.4% 11.2%)',
+        },
+        destructive: {
+          DEFAULT: 'hsl(0 100% 50%)',
+          foreground: 'hsl(210 40% 98%)',
+        },
+        muted: {
+          DEFAULT: 'hsl(210 40% 96.1%)',
+          foreground: 'hsl(215.4 16.3% 46.9%)',
+        },
+        accent: {
+          DEFAULT: 'hsl(210 40% 96.1%)',
+          foreground: 'hsl(222.2 47.4% 11.2%)',
+        },
+        popover: {
+          DEFAULT: 'hsl(0 0% 100%)',
+          foreground: 'hsl(222.2 47.4% 11.2%)',
+        },
+        card: {
+          DEFAULT: 'hsl(0 0% 100%)',
+          foreground: 'hsl(222.2 47.4% 11.2%)',
+        },
       },
-    },
-    fontFamily: {
-      sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      serif: ['Inter', ...defaultTheme.fontFamily.serif],
+      borderRadius: {
+        lg: `0.5rem`,
+        md: `calc(0.5rem - 2px)`,
+        sm: 'calc(0.5rem - 4px)',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Inter', ...defaultTheme.fontFamily.serif],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      // animation: {
+      //   'accordion-down': 'accordion-down 0.2s ease-out',
+      //   'accordion-up': 'accordion-up 0.2s ease-out',
+      // },
     },
     screens: {
       sm: '640px',
@@ -32,5 +82,5 @@ module.exports = {
       padding: '1rem',
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+const config = require('./docs/config/tailwind.config');
+
+const { content, ...rest } = config;
+
+module.exports = rest;
